982e8db474256f5191641d8f31c7e1a951b26b3b
Foysal-Munsy <bdfoysal555@gmail.com>
2025-12-09 07:40:00 +0600

    [added] inter font family added.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Sidebar1 from "@/components/layout/Sidebar-1";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/layout/app-sidebar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -28,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-[#111953]`}
-      >
+      <body className={`${inter.variable} bg-[#111953] font-sans`}>
         <div className="flex min-h-screen flex-col">
           <Navbar />
           <SidebarProvider>
