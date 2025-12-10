aae87920e6c542d2c5e268a0888cad0571adb748
Foysal-Munsy <bdfoysal555@gmail.com>
2025-12-10 08:05:05 +0600

    [added] added dynamic call history card list.

--- components/chat/calls/CallsContent.tsx
+++ components/chat/calls/CallsContent.tsx
@@ -1,71 +1,21 @@
 import { SidebarHeader } from "@/components/ui/sidebar";
-import { CallMinus, Sort } from "iconsax-reactjs";
+import { Sort } from "iconsax-reactjs";
+import HistoryCard from "./HistoryCard";
 
-import Image from "next/image";
-
-const posts = [
-  {
-    id: 1,
-    image: "https://i.ibb.co.com/6c782gQh/dms-image.png",
-    name: "Foysal Munsy",
-    call: "Missed In-Coming Call",
-    date: "31 Aug 2025",
-    action: true,
-    status: "active",
-  },
-];
 export default async function CallsContent() {
   return (
     <div className=" min-h-[85vh] m-4 bg-white border rounded-md w-[827px] ">
-      <SidebarHeader className="text-[18px] flex flex-row justify-between items-center font-semibold text-[#191F38]">
-        History
-        <div className="border p-1 rounded-md">
-          <Sort size="16" color="#4157FE" />
-        </div>
-      </SidebarHeader>
-      {/* <SidebarContent className="grid grid-cols-1 gap-2 "> */}
-      {/* card */}
-      <div className="px-2">
-        <div className="flex border border-[#EBEBEB] p-2 rounded-md">
-          {/* image */}
-          <div className="flex gap-2">
-            <div className="border-r-2 border-[#DC2626] h-[50px]"></div>
-            <div className="relative h-11 w-11 shrink-0">
-              <Image
-                className="rounded-full object-cover"
-                src={posts[0].image}
-                alt={"image"}
-                fill
-                sizes="44px"
-              />
-              {posts[0].status === "active" && (
-                <span className="absolute bottom-0 right-0 block h-3 w-3 rounded-full border border-white bg-[#22C55E]" />
-              )}
-            </div>
+      <div className="w-[796px] mx-auto">
+        <SidebarHeader className="text-[18px] flex flex-row justify-between items-center font-semibold text-[#191F38] mb-[17px]">
+          History
+          <div className="border p-1 rounded-md">
+            <Sort size="16" color="#4157FE" />
           </div>
+        </SidebarHeader>
 
-          {/* content */}
-          <div className="flex-1 pl-2">
-            <div className="flex justify-between items-center w-full">
-              <h3 className="text-[#191F38] font-semibold text-[14px]">
-                {posts[0].name}
-              </h3>
-              <p className="font-medium text-[14px] text-[#697588]">
-                {posts[0].date}
-              </p>
-            </div>
-            {/* calls action */}
-            <div>
-              {posts[0].call === "Missed In-Coming Call" && (
-                <p className="text-[#DC2626] font-medium text-[14px] flex gap-2 items-center">
-                  Missed In-Coming Call <CallMinus size={16} />
-                </p>
-              )}
-            </div>
-          </div>
-        </div>
+        {/* card */}
+        <HistoryCard />
       </div>
-      {/* </SidebarContent> */}
     </div>
   );
 }
---
+++ components/chat/calls/HistoryCard.tsx
@@ -0,0 +1,141 @@
+import { CallIncoming, CallMinus, CallOutgoing } from "iconsax-reactjs";
+import Image from "next/image";
+
+const posts = [
+  {
+    id: 1,
+    image: "/assets/Avatar1.png",
+    name: "Foysal Munsy",
+    call: "Missed In-Coming Call",
+    date: "31 Aug 2025",
+    action: "missed",
+    status: "green",
+    line: "red",
+  },
+  {
+    id: 2,
+    image: "/assets/Avatar1.png",
+    name: "Foysal Munsy",
+    call: "Out-Going",
+    duration: "3m 21s",
+    date: "31 Aug 2025",
+    action: "out",
+    status: "gray",
+    line: "green",
+  },
+  {
+    id: 3,
+    image: "/assets/Avatar1.png",
+    name: "Foysal Munsy",
+    call: "In-Coming",
+    duration: "3m 21s",
+    date: "31 Aug 2025",
+    action: "in",
+    status: "orange",
+    line: "green",
+  },
+  {
+    id: 4,
+    image: "/assets/svg/groups-frame.svg",
+    name: "Pat Cummins. Will Smith, Bruce Wayne",
+    call: "In-Coming",
+    duration: "3m 21s",
+    date: "31 Aug 2025",
+    action: "in",
+    status: "",
+    line: "violet",
+  },
+  {
+    id: 5,
+    image: "/assets/svg/groups-frame.svg",
+    name: "Pat Cummins. Will Smith, Bruce Wayne",
+    call: "In-Coming",
+    duration: "3m 21s",
+    date: "31 Aug 2025",
+    action: "in",
+    status: "",
+    line: "violet",
+  },
+  {
+    id: 6,
+    image: "/assets/Avatar1.png",
+    name: "Foysal Munsy",
+    call: "In-Coming",
+    duration: "3m 21s",
+    date: "31 Aug 2025",
+    action: "in",
+    status: "orange",
+    line: "green",
+  },
+];
+export default function HistoryCard() {
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id} className="mb-2.5">
+          <div className="flex gap-2 items-center border border-[#EBEBEB] rounded-md p-3 h-[68px]">
+            {/* image */}
+            <div className="flex gap-2">
+              {post.line === "red" ? (
+                <div className="border-r-2 border-[#DC2626] h-[50px]"></div>
+              ) : post.line === "green" ? (
+                <div className="border-r-2 border-[#059669] h-[50px]"></div>
+              ) : (
+                <div className="border-r-2 border-[#722BCC] h-[50px]"></div>
+              )}
+
+              <div className="relative h-11 w-11 shrink-0">
+                <Image
+                  className="rounded-full object-cover"
+                  src={post.image}
+                  alt={"image"}
+                  fill
+                  sizes="44px"
+                />
+                {post?.status === "green" && (
+                  <span className="absolute bottom-0 right-0 block h-3 w-3 rounded-full border border-white bg-[#22C55E]" />
+                )}
+                {post?.status === "gray" && (
+                  <span className="absolute bottom-0 right-0 block h-3 w-3 rounded-full border border-white bg-[#697588]" />
+                )}
+                {post?.status === "orange" && (
+                  <span className="absolute bottom-0 right-0 block h-3 w-3 rounded-full border border-white bg-[#FF9F00]" />
+                )}
+              </div>
+            </div>
+
+            <div className="flex flex-1 justify-between">
+              {/* left content */}
+              <div className="flex flex-col items-start">
+                <h3 className="text-[#191F38] font-semibold text-[14px]">
+                  {post.name}
+                </h3>
+                {post.action === "missed" ? (
+                  <p className="text-[#DC2626] font-medium text-[14px] flex gap-2 items-center">
+                    {post.call} <CallMinus size={16} />
+                  </p>
+                ) : post.action === "out" ? (
+                  <p className="text-[#059669] font-medium text-[14px] flex gap-2 items-center">
+                    {post.call} <CallOutgoing size={16} />
+                  </p>
+                ) : (
+                  <p className="text-[#722BCC] font-medium text-[14px] flex gap-2 items-center">
+                    {post.call} <CallIncoming size={16} />
+                  </p>
+                )}
+              </div>
+
+              {/* right content */}
+              <div className="text-[#697588] flex flex-col items-end">
+                <p className="font-medium text-[14px] ">{post.date}</p>
+                {post?.duration && (
+                  <p className="font-medium text-[14px] ">{post.duration}</p>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
