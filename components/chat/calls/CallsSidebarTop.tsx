18edc8c39e06ea90cb99d3a89eea5962bc0e3ec9
Foysal-Munsy <bdfoysal555@gmail.com>
2025-12-11 08:51:47 +0600

    [added] input group and dropdown menu with interaction to the calls section.

--- components/chat/calls/CallsSidebarTop.tsx
+++ components/chat/calls/CallsSidebarTop.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Sidebar,
   SidebarContent,
@@ -6,15 +7,23 @@ import {
   SidebarGroupContent,
   SidebarHeader,
 } from "@/components/ui/sidebar";
-import { Setting2 } from "iconsax-reactjs";
-import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Setting2, TickCircle } from "iconsax-reactjs";
 import { Button } from "@/components/ui/button";
+import { Search, X } from "lucide-react";
+import { InputGroup, InputGroupAddon } from "@/components/ui/input-group";
 
 export default function CallsSidebarTop() {
   return (
     <Sidebar
       collapsible="none"
-      className=" h-[232px] m-4 bg-[#FDFDFD] border rounded-md w-[323px] "
+      className=" h-[232px] m-4 bg-[#FDFDFD] border rounded-[12px] w-[323px] p-[15px]"
     >
       <SidebarHeader className="text-[18px] font-semibold text-[#191F38]">
         Search Friends to call
@@ -22,7 +31,20 @@ export default function CallsSidebarTop() {
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupContent className="grid grid-cols-1 gap-4 ">
-            <Input placeholder="search..." />
+            <InputGroup>
+              <InputGroupAddon>
+                <Search />
+                <button className="w-[122px] h-[25px] border border-[#4157FE33] rounded-[6px] bg-[#F2F9FE] p-1.5 flex items-center gap-1 whitespace-nowrap">
+                  <span className="bg-[#4157FE] w-[17px] h-[17px] rounded-full flex items-center justify-center text-white p-1.5 font-[400px] text-[10.2px] ">
+                    F
+                  </span>
+                  <p className="text-[#191F38] font-medium text-[12px] truncate">
+                    Foysal Munsy
+                  </p>
+                  <X size={16} color="#191F38" />
+                </button>
+              </InputGroupAddon>
+            </InputGroup>
             <Button className="bg-[#4157FE] rounded-md hover:bg-[#4135FE]">
               Call
             </Button>
@@ -30,13 +52,45 @@ export default function CallsSidebarTop() {
         </SidebarGroup>
       </SidebarContent>
       <SidebarFooter>
-        <Button
-          className="w-[123px] text-[14px] rounded-md bg-[#EBEBEB] text-[#697588]"
-          variant={"outline"}
-        >
-          <Setting2 />
-          Default
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              className="w-[130px] h-8 text-[14px] font-medium border-[#EBEBEB]  bg-white text-[#697588] p-1 rounded-md"
+              variant={"outline"}
+            >
+              <Setting2 />
+              Default Setup
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            align="end"
+            className="w-[194px] min-h-[143px] rounded-xl border p-2 text-[#697588] font-medium text-[14px] "
+          >
+            <DropdownMenuLabel className="text-[16px] font-medium text-[#191F38] pl-[15px]">
+              Audio Devices
+            </DropdownMenuLabel>
+            {/* <DropdownMenuSeparator /> */}
+
+            <DropdownMenuItem
+              className={`text-[14px] font-medium pl-[15px] py-2.5 rounded-none `}
+            >
+              Audio Devices 1
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className={`text-[14px] text-[#4157FE] font-medium pl-[15px] py-2.5 rounded-none flex justify-between`}
+            >
+              <span>Default Setup</span>
+              <TickCircle size={16} color="#4157FE" />
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className={`text-[14px] font-medium pl-[15px] py-2.5 rounded-none cursor-pointer`}
+              onClick={() => (window.location.href = "/dashboard")}
+            >
+              <Setting2 />
+              Device Setting
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </SidebarFooter>
     </Sidebar>
   );
